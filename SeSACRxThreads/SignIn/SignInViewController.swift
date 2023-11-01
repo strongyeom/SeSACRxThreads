e83cf8ff47b8306e0e100a5f66a6a3a98e1d5410
yeomsungpil <present7256@gmail.com>
2023-11-01 10:21:27 +0900

    UI에 특화되어 있을때  UIKit -> Rx 코드로 변환

--- SeSACRxThreads/SignIn/SignInViewController.swift
+++ SeSACRxThreads/SignIn/SignInViewController.swift
@@ -11,7 +11,7 @@ import RxCocoa
 import RxSwift
 
 class SignInViewController: UIViewController {
-
+    
     let emailTextField = SignTextField(placeholderText: "이메일을 입력해주세요")
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let signInButton = PointButton(title: "로그인")
@@ -20,14 +20,14 @@ class SignInViewController: UIViewController {
     // 기존 스위치 버튼 만드는 방법
     let test = UISwitch()
     
-    // 데이터 기반으로 적용 : 이벤트 생성, 전달 <- 이벤트를 처리 할 수는 없음 즉, 이벤트를 받을 수 없음 
+    // 데이터 기반으로 적용 : 이벤트 생성, 전달 <- 이벤트를 처리 할 수는 없음 즉, 이벤트를 받을 수 없음
     // 이벤트를 전달과 처리를 같이 하기 위해 Subject 사용
     let isOn =  BehaviorSubject(value: true)    // Observable.of(true)
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         view.backgroundColor = Color.white
         
         testSwitch()
@@ -51,20 +51,16 @@ class SignInViewController: UIViewController {
             }
             .disposed(by: disposeBag)
         
-                // 버튼을 만들지 않고 타이머를 통해 꺼지는것 확인하기 위한 DispatchQueue
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    
-                    self.isOn.onNext(false) // <- isOn : Observer 역할
-                }
+        isOn // rx 스러운 코드
+            .bind(to: test.rx.isOn)
+            .disposed(by: disposeBag)
         
+        // 버튼을 만들지 않고 타이머를 통해 꺼지는것 확인하기 위한 DispatchQueue
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            
+            self.isOn.onNext(false) // <- isOn : Observer 역할
+        }
         
-        // setOn: default로 설정할 수 있는 메서드 ex) 스위치 ON인 상태로 하고싶음
-//        test.setOn(true, animated: false)
-//
-//        // 버튼을 만들지 않고 타이머를 통해 꺼지는것 확인하기 위한 DispatchQueue
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-//            self.test.setOn(false, animated: false)
-//        }
     }
     
     @objc func signUpButtonClicked() {
@@ -108,5 +104,5 @@ class SignInViewController: UIViewController {
         }
     }
     
-
+    
 }
