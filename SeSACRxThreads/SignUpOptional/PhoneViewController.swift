2107442fcdd2acd805f43dfdce3ddce948c8cd70
yeomsungpil <present7256@gmail.com>
2023-11-01 11:32:52 +0900

    TextField 갯수에 따른 Subject를 활용하여 이벤트 전달 및 처리를 사용하여 적용 / 메모리 누수 잡기  구버전 : withUnretained를 사용 ->  최신 버전 : Subscribe(with ~ )

--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -18,6 +18,7 @@ class PhoneViewController: UIViewController {
     // 보여줄때 처음 초기값 보여주게 하기
     let phone = BehaviorSubject(value: "010")
     let buttonColor = BehaviorSubject(value: UIColor.red)
+    let buttonEnabled = BehaviorSubject(value: false)
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
@@ -34,6 +35,12 @@ class PhoneViewController: UIViewController {
     
     func bind() {
         
+        // 초기에 false로 버튼 isEnabled 설정하기
+        buttonEnabled
+            .bind(to: nextButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        
         buttonColor
             .bind(to: nextButton.rx.backgroundColor, phoneTextField.rx.tintColor)
             .disposed(by: disposeBag)
@@ -44,9 +51,59 @@ class PhoneViewController: UIViewController {
             .bind(to: phoneTextField.layer.rx.borderColor)
             .disposed(by: disposeBag)
         
-        phone
+        
+        phone // 전달하는 목적일때도 있고 처리 할때도 쓰일때가 있기 때문에 Subject로 묶었음
             .bind(to: phoneTextField.rx.text)
             .disposed(by: disposeBag)
+        
+        phone
+            .map { $0.count > 10 }
+            .subscribe { value in
+                print("=== \(value)")
+                let color = value ? UIColor.blue : UIColor.red
+                self.buttonColor.onNext(color)
+//                self.nextButton.isEnabled = value
+                self.buttonEnabled.onNext(value) // ==  self.buttonEnabled.on(.next(value)) 같음
+                
+            }
+            .disposed(by: disposeBag)
+        
+        // 클로저 안에 self 처리 1.
+        phone
+            .map { $0.count > 10 }
+            .withUnretained(self) // 클로저 구문 안에 self를 안적게 도와줌 == [weak self] 대치
+        // withUnretained를 사용하게 되면 매개변수로 (VC, Bool)이 되기 때문에 클로저 구문안에 매개변수로 하나더 생성하기
+            .subscribe { object, value in
+                print("=== \(value)")
+                let color = value ? UIColor.blue : UIColor.red
+                object.buttonColor.onNext(color)
+//                self.nextButton.isEnabled = value
+                object.buttonEnabled.onNext(value) // ==  self.buttonEnabled.on(.next(value)) 같음
+                
+            }
+            .disposed(by: disposeBag)
+        
+        // 클로저 안에 self 처리 2.
+        phone
+            .map { $0.count > 10 }
+            // subscribe(with ~ ) : 내부에 withUnretained 를 적용해줌
+            .subscribe(with: self, onNext: { owner, value in
+                print("=== \(value)")
+                let color = value ? UIColor.blue : UIColor.red
+                owner.buttonColor.onNext(color)
+                owner.buttonEnabled.onNext(value) // ==  self.buttonEnabled.on(.next(value)) 같음
+            })
+            .disposed(by: disposeBag)
+        
+        phoneTextField.rx.text.orEmpty
+            .subscribe { value in
+                let result = value.formated(by: "###-###-####")
+                print("result : \(result), value: \(value)")
+                self.phone.onNext(result)
+            }
+            .disposed(by: disposeBag)
+        
+        
     }
     
     @objc func nextButtonClicked() {
@@ -73,3 +130,42 @@ class PhoneViewController: UIViewController {
 
 }
 
+extension String {
+    var decimalFilteredString: String {
+        return String(unicodeScalars.filter(CharacterSet.decimalDigits.contains))
+    }
+    
+    func formated(by patternString: String) -> String {
+        let digit: Character = "#"
+ 
+        let pattern: [Character] = Array(patternString)
+        let input: [Character] = Array(self.decimalFilteredString)
+        var formatted: [Character] = []
+ 
+        var patternIndex = 0
+        var inputIndex = 0
+ 
+        // 2
+        while inputIndex < input.count {
+            let inputCharacter = input[inputIndex]
+ 
+            // 2-1
+            guard patternIndex < pattern.count else { break }
+ 
+            switch pattern[patternIndex] == digit {
+            case true:
+                // 2-2
+                formatted.append(inputCharacter)
+                inputIndex += 1
+            case false:
+                // 2-3
+                formatted.append(pattern[patternIndex])
+            }
+ 
+            patternIndex += 1
+        }
+ 
+        // 3
+        return String(formatted)
+    }
+}
