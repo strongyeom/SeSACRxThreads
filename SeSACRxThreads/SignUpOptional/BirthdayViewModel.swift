19b185b5bdb654a59177b646d1bde0d290cabb82
yeomsungpil <present7256@gmail.com>
2023-11-03 12:33:34 +0900

    Realy 사용 할때는 accept을 사용  but, UI 관련한거는 Relay를 사용할때도 있음 subject 개념만 확실히 있으면 크게 어려울거 없음

--- SeSACRxThreads/SignUpOptional/BirthdayViewModel.swift
+++ SeSACRxThreads/SignUpOptional/BirthdayViewModel.swift
@@ -7,15 +7,18 @@
 
 import Foundation
 import RxSwift
-// import RxCocoa  UIKit 기반이긴 하지만 구성하다보면 import 할 때도 있음 
+import RxCocoa
+
 class BirthdayViewModel {
     
     // 데이터 기반으로 생각 해야함 년, 월, 일 , datePicker에 오늘 날짜를 띄어줄지 초기값 설정
     
     let birthday: BehaviorSubject<Date> = BehaviorSubject(value: .now)
-    let year =  BehaviorSubject(value: 2023) // Observable.of(2020)
-    let month = BehaviorSubject(value: 12)
-    let day = BehaviorSubject(value: 25)
+    let year =  BehaviorRelay(value: 2023) // Observable.of(2020)
+    let month = BehaviorRelay(value: 12)
+    let day = BehaviorRelay(value: 25)
+    
+
     
     let disposeBag = DisposeBag()
     
@@ -26,9 +29,12 @@ class BirthdayViewModel {
                 let component = Calendar.current.dateComponents([.year, .month, .day], from: date)
                 
                 // 주‼️ 직접적으로 넣어주는게 아님!!! 전달 -> 전달 -> 전달 ‼️의
-                owner.year.onNext(component.year!) // year Observer
-                owner.month.onNext(component.month!)
-                owner.day.onNext(component.day!)
+                owner.year.accept(component.year!) // year Observer
+                owner.month.accept(component.month!)
+                owner.day.accept(component.day!)
+                
+                
+                
             }
             .disposed(by: disposeBag)
     }
