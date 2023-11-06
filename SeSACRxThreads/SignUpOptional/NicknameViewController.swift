362a82b5f1cc23948235049d245fcbf973347358
yeomsungpil <present7256@gmail.com>
2023-11-06 12:21:41 +0900

    share 기능이 내장되어 있는 연산자 Driver : 스트림 공유, 메인쓰레드에서 동작하는것을 보장 / UI에 특화

--- SeSACRxThreads/SignUpOptional/NicknameViewController.swift
+++ SeSACRxThreads/SignUpOptional/NicknameViewController.swift
@@ -35,23 +35,27 @@ class NicknameViewController: UIViewController {
         // map의 return값이 Observable<Result> 으로 되어 있기 때문에 Unicast로 보여짐 -> Observer의 갯수만큼 탐
         // ==> 해결 방법 .share()을 사용하여 Muticast의 기능을 할 수있게 해줌
             .map{ "안녕하세요 반갑습니다. \(Int.random(in: 1...100))" }
-            .share()
+            // ⭐️ drvie에는 share()가 내장되어 있음 ...
+            // map은 return Observable 함 --> return Driver로 바꿔줌 <- UI에 특화
+            // UI에 특화되어 있기 때문에 메인쓰레드 작동, error 발생하지 않음
+            // 에러가 발생하면 어떻게? onErrorJustReturn: "" 여기에 에러 발생했을때 적으면됨
+            .asDriver(onErrorJustReturn: "")
         
         tap
-            .subscribe(with: self) { owner, value in
+            .drive(with: self) { owner, value in
                 owner.nextButton.setTitle(value, for: .normal)
             }
             .disposed(by: disposeBag)
         
         tap
         // bind : UI에 적합한 형태 next만 방출함
-            .bind(with: self) { owner, value in
+            .drive(with: self) { owner, value in
                 owner.nicknameTextField.text = value
             }
             .disposed(by: disposeBag)
         
         tap
-            .bind(with: self) { owner, value in
+            .drive(with: self) { owner, value in
                 owner.navigationItem.title = value
             }
             .disposed(by: disposeBag)
